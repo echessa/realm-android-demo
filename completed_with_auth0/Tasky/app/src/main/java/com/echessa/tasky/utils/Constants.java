f57ad929d8ecd526faf5c1c608afa6c2faf6b794
Joyce Echessa <jokhessa@yahoo.com>
2017-04-19 16:16:45 +0300

    Add updated project files

---
+++ completed_with_auth0/Tasky/app/src/main/java/com/echessa/tasky/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.echessa.tasky.utils;
+
+/**
+ * Created by echessa on 4/19/17.
+ */
+
+class Constants {
+    final static String REFRESH_TOKEN = "refresh_token";
+    final static String ACCESS_TOKEN = "access_token";
+    final static String ID_TOKEN = "id_token";
+    final static String TOKEN_TYPE = "token_type";
+    final static String EXPIRES_IN = "expires_in";
+}
