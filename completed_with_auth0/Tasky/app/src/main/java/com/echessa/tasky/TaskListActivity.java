f4128fdf859a12b9b275e1e25d43b03b33dd4165
Joyce Echessa <jokhessa@yahoo.com>
2017-05-04 20:03:22 +0300

    Add updated project files

---
+++ completed_with_auth0/Tasky/app/src/main/java/com/echessa/tasky/TaskListActivity.java
@@ -0,0 +1,181 @@
+package com.echessa.tasky;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.echessa.tasky.models.Task;
+import com.echessa.tasky.utils.CredentialsManager;
+
+import java.util.UUID;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+public class TaskListActivity extends AppCompatActivity {
+
+    private Realm realm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+
+        realm = Realm.getDefaultInstance();
+
+        // RealmResults are "live" views, that are automatically kept up to date, even when changes happen
+        // on a background thread. The RealmBaseAdapter will automatically keep track of changes and will
+        // automatically refresh when a change is detected.
+        RealmResults<Task> tasks = realm.where(Task.class).findAll();
+        tasks = tasks.sort("timestamp");
+        final TaskAdapter adapter = new TaskAdapter(this, tasks);
+
+        ListView listView = (ListView) findViewById(R.id.task_list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final Task task = (Task) adapterView.getAdapter().getItem(i);
+                final EditText taskEditText = new EditText(TaskListActivity.this);
+                taskEditText.setText(task.getName());
+                AlertDialog dialog = new AlertDialog.Builder(TaskListActivity.this)
+                        .setTitle("Edit Task")
+                        .setView(taskEditText)
+                        .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                changeTaskName(task.getId(), String.valueOf(taskEditText.getText()));
+                            }
+                        })
+                        .setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                deleteTask(task.getId());
+                            }
+                        })
+                        .create();
+                dialog.show();
+            }
+        });
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final EditText taskEditText = new EditText(TaskListActivity.this);
+                AlertDialog dialog = new AlertDialog.Builder(TaskListActivity.this)
+                        .setTitle("Add Task")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                realm.executeTransactionAsync(new Realm.Transaction() {
+                                    @Override
+                                    public void execute(Realm realm) {
+                                        Task task = realm.createObject(Task.class, UUID.randomUUID().toString());
+                                        task.setName(String.valueOf(taskEditText.getText()));
+                                        task.setTimestamp(System.currentTimeMillis());
+                                    }
+                                });
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        realm.close();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_task_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_delete) {
+            deleteAllDone();
+            return true;
+        } else if (id == R.id.logout) {
+            logout();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void changeTaskDone(final String taskId) {
+        realm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Task task = realm.where(Task.class).equalTo("id", taskId).findFirst();
+                task.setDone(!task.isDone());
+            }
+        });
+    }
+
+    private void changeTaskName(final String taskId, final String name) {
+        realm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Task task = realm.where(Task.class).equalTo("id", taskId).findFirst();
+                task.setName(name);
+            }
+        });
+    }
+
+    private void deleteTask(final String taskId) {
+        realm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                realm.where(Task.class).equalTo("id", taskId)
+                        .findFirst()
+                        .deleteFromRealm();
+            }
+        });
+    }
+
+    private void deleteAllDone() {
+        realm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                realm.where(Task.class).equalTo("done", true)
+                        .findAll()
+                        .deleteAllFromRealm();
+            }
+        });
+    }
+
+    private void logout() {
+        CredentialsManager.deleteCredentials(TaskListActivity.this);
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+}
---
+++ completed_with_auth0/Tasky/app/src/main/java/com/echessa/tasky/utils/CredentialsManager.java
@@ -0,0 +1,58 @@
+package com.echessa.tasky.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.auth0.android.result.Credentials;
+import com.echessa.tasky.R;
+
+import static com.echessa.tasky.utils.Constants.ACCESS_TOKEN;
+import static com.echessa.tasky.utils.Constants.EXPIRES_IN;
+import static com.echessa.tasky.utils.Constants.ID_TOKEN;
+import static com.echessa.tasky.utils.Constants.REFRESH_TOKEN;
+import static com.echessa.tasky.utils.Constants.TOKEN_TYPE;
+
+/**
+ * Created by echessa on 5/4/17.
+ */
+
+public class CredentialsManager {
+
+    public static void saveCredentials(Context context, Credentials credentials) {
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                context.getString(R.string.auth0_preferences), Context.MODE_PRIVATE);
+
+        sharedPref.edit()
+                .putString(ID_TOKEN, credentials.getIdToken())
+                .putString(REFRESH_TOKEN, credentials.getRefreshToken())
+                .putString(ACCESS_TOKEN, credentials.getAccessToken())
+                .putString(TOKEN_TYPE, credentials.getType())
+                .putLong(EXPIRES_IN, credentials.getExpiresIn())
+                .apply();
+    }
+
+    public static Credentials getCredentials(Context context) {
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                context.getString(R.string.auth0_preferences), Context.MODE_PRIVATE);
+
+        return new Credentials(
+                sharedPref.getString(ID_TOKEN, null),
+                sharedPref.getString(ACCESS_TOKEN, null),
+                sharedPref.getString(TOKEN_TYPE, null),
+                sharedPref.getString(REFRESH_TOKEN, null),
+                sharedPref.getLong(EXPIRES_IN, 0));
+    }
+
+    public static void deleteCredentials(Context context) {
+        SharedPreferences sharedPref = context.getSharedPreferences(
+                context.getString(R.string.auth0_preferences), Context.MODE_PRIVATE);
+
+        sharedPref.edit()
+                .putString(ID_TOKEN, null)
+                .putString(REFRESH_TOKEN, null)
+                .putString(ACCESS_TOKEN, null)
+                .putString(TOKEN_TYPE, null)
+                .putLong(EXPIRES_IN, 0)
+                .apply();
+    }
+}
